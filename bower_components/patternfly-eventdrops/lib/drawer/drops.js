e60674830d62bf3c73ccccc3fca02981b191b2b5
Chris Shinn <me@cshinn.com>
2016-06-13 10:42:12 -0400

    Update to match visual design

--- bower_components/patternfly-eventdrops/lib/drawer/drops.js
+++ bower_components/patternfly-eventdrops/lib/drawer/drops.js
@@ -10,13 +10,15 @@ export default (svg, scales, configuration) => function dropsSelector(data) {
     dropLines.each(function dropLineDraw(drop) {
         const drops = d3.select(this).selectAll('.drop').data(drop.data);
 
-        drops.attr("transform", function (d) { return "translate(" + (scales.x(d.date) + 200) + "," + 3 + ")"; });
+        drops.attr("transform", function (d) { return "translate(" + (scales.x(d.date) + 200) + "," + -3 + ")"; });
 
         const shape = drops.enter()
             .append('text')
             .classed('drop', true)
-            .attr("transform", function (d) {return "translate(" + (scales.x(d.date) + 200) + "," + 3 + ")"; })
+            .attr("transform", function (d) {return "translate(" + (scales.x(d.date) + 200) + "," + -3 + ")"; })
             .attr('fill', configuration.eventColor)
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'middle')
             .attr('data-toggle', 'tooltip')
             .attr('title', configuration.eventTooltip)
             .text(configuration.eventShape);
