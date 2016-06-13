7b3f91e94b4f704dc5dd380320ff6702db6da034
Chris Shinn <me@cshinn.com>
2016-06-13 10:42:19 -0400

    Update to match visual design

--- bower_components/patternfly-eventdrops/lib/drawer/marker.js
+++ bower_components/patternfly-eventdrops/lib/drawer/marker.js
@@ -1,11 +1,12 @@
 import d3 from 'd3';
 
-export default (gridContainer, dimensions) => {
+export default (gridContainer, stampContainer, scales, dimensions, dateFormat) => {
     gridContainer.append('rect')
             .attr('width', dimensions.width)
             .attr('height', dimensions.height - 1)
-            .on("mouseover", function() { marker.style("display", null); })
-            .on("mouseout", function() { marker.style("display", "none"); })
+            .attr('fill', 'url(#gridStripes)')
+            .on("mouseover", function() { marker.style("display", null); timeStamp.style("display", null); timeBox.style("display", null);})
+            .on("mouseout", function() { marker.style("display", "none"); timeStamp.style("display", "none"); timeBox.style("display", "none"); })
             .on('mousemove', moveMarker);
 
 
@@ -14,8 +15,32 @@ export default (gridContainer, dimensions) => {
             .attr('y1', 0)
             .attr('y2', dimensions.height);
 
+    const domain = scales.x.domain();
+/*
+    extremum.append('text')
+        .text(dateFormat(domain[0]))
+        .classed('minimum', true);
+*/
+    var timeBox = stampContainer.append('rect')
+        .attr('height', '24')
+        .attr('width', '130')
+        .style('display', 'none');
+
+    var timeStamp = stampContainer.append('text')
+        .text(dateFormat(domain[1]))
+        .attr('transform', `translate(${scales.x.range()[1]})`)
+        .attr('text-anchor', 'middle');
+
+
+
+
 function moveMarker() {
-    marker.attr('transform', `translate(${d3.mouse(gridContainer[0][0])[0]},0)`);
+    var pos = d3.mouse(gridContainer[0][0])[0]
+    marker.attr('transform', `translate(${pos})`);
+    timeBox.attr('transform', `translate(${pos - 65}, -20)`);
+    timeStamp.attr('transform', `translate(${pos}, -4)`)
+        .text(dateFormat(scales.x.invert(pos)));
+
 }
 
 };
